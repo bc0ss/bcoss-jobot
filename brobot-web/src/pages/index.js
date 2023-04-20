cb5fedb9272610c81652ca0662197036fe5129e8
brianc <brian.coss@broadcom.com>
2023-04-20 14:50:42 -0600

    modified:   brobot-web/src/pages/index.js

--- brobot-web/src/pages/index.js
+++ brobot-web/src/pages/index.js
@@ -1,6 +1,30 @@
-export default function Home() {
-  return(
-    <div>hello world</div>
-  )
+javascript
+import { useState } from "react";
 
+export default function Home() {
+  const [apiKey, setApiKey] = useState("");
+
+  /* Add more logic here */
+
+  return (
+    <div className="flex flex-col h-screen">
+      {/* Navbar */}
+      <nav className="bg-white shadow w-full">
+        <div className="px-4 h-14 flex justify-between items-center">
+          <div className="text-xl font-bold">Jobot</div>
+          <div>
+            <input
+              type="password"
+              className="border rounded p-1"
+              placeholder="Enter API key.."
+              value={apiKey}
+              onChange={(e) => setApiKey(e.target.value)}
+            />
+          </div>
+        </div>
+      </nav>
+
+      {/* Add more UI here */}
+    </div>
+  );
 }
