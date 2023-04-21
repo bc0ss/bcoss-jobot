268e84d9d8e42a02f938daa88d558bafd08e02b3
bc0ss <98187993+bc0ss@users.noreply.github.com>
2023-04-21 18:10:28 +0000

    updates for api key

--- brobot-web/src/pages/index.js
+++ brobot-web/src/pages/index.js
@@ -1,10 +1,26 @@
-javascript
 import { useState } from "react";
 
 export default function Home() {
   const [apiKey, setApiKey] = useState("");
+  const API_URL = "https://api.openai.com/v1/chat/completions";
 
-  /* Add more logic here */
+  async function sendRequest(){
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + apiKey,
+      },
+      body: JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Hellow!"}]
+      })
+    })
+  return response
+  }
+
+   
+  /* Add  more logic here */
 
   return (
     <div className="flex flex-col h-screen">
@@ -24,7 +40,11 @@ export default function Home() {
         </div>
       </nav>
 
-      {/* Add more UI here */}
+      <div className="p-4">
+        <button className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600 text-white">
+          Send Request
+        </button>
+      </div>
     </div>
   );
 }
