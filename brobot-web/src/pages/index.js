d4daf59a721ef53b735092cb65536a7b3052dedb
bc0ss <98187993+bc0ss@users.noreply.github.com>
2023-04-21 21:24:37 +0000

    working proof of concept

--- brobot-web/src/pages/index.js
+++ brobot-web/src/pages/index.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [apiKey, setApiKey] = useState("");
+  const SYSTEM_MESSAGE="You are Brobot an extremely sarcastic but helpful AI built with state of the art large language models"
+   const [apiKey, setApiKey] = useState("");
+  const [messages, setMessages] = useState([
+            {"role": "system", "content": SYSTEM_MESSAGE},
+          ])
+  const [userMessage, setUserMessage] = useState("");
+  
   const API_URL = "https://api.openai.com/v1/chat/completions";
 
+
   async function sendRequest(){
+    // update the message history
+const newMessage = { role: "user", content: userMessage};
+const newMessages = [  ...messages, newMessage];
+setMessages(newMessages);
+setUserMessage("");
+
     const response = await fetch(API_URL, {
       method: 'POST',
       headers: {
@@ -13,12 +26,19 @@ export default function Home() {
       },
       body: JSON.stringify({
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": "Hellow!"}]
-      })
-    })
-  return response
+        "messages": newMessages,
+      }),
+    });
+    const responseJson = await response.json();
+    
+    const newBotMessage = responseJson.choices[0].message;
+
+    const newMessages2 = [...newMessages, newBotMessage];
+
+    setMessages(newMessages2);
   }
 
+  
    
   /* Add  more logic here */
 
@@ -40,12 +60,33 @@ export default function Home() {
         </div>
       </nav>
 
-      <div className="p-4">
-        <button className="border rounded-md p-2 bg-blue-500 hover:bg-blue-600 text-white"
-        ONCLICK={sendRequest}>
-          Send Request
-        </button>
+      {/*Message History*/} 
+      <div className="flex-1">
+          <div className="w-full max-w-screen-md mx-auto" >
+            {messages.filter(message => message.role!=="system").map((message,idx) => (
+            <div key={idx} className="mt-3">
+              <div className="font-bold">
+                {message.role === "user" ? "You" : "Brobot"}
+              </div>
+              <div className="text-lg">{message.content}</div>
+            </div>
+            ))}
+          </div>
       </div>
-    </div>
+      {/* Message Input Box*/}
+      <div>
+        <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4">
+          <textarea 
+          value={userMessage}
+          onChange={(e) => setUserMessage(e.target.value)}
+          className="border text-lg rounded-md p-1 flex-1" rows={1}/>
+          <button
+          onClick={sendRequest} 
+          className="bg-green-500 hoaver:bg-green-600 border rounded-md text-white text-lg w-20 p-2 ml-2">
+            Send
+          </button>
+        </div>
+      </div>
+   </div>
   );
 }
