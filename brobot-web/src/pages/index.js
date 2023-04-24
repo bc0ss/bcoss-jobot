127d998b101c5f30a9e9db30b5d85f7e3f1ff21a
bc0ss <98187993+bc0ss@users.noreply.github.com>
2023-04-24 04:42:36 +0000

    chapter 1 complete

--- brobot-web/src/pages/index.js
+++ brobot-web/src/pages/index.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import Head from "next/head";
+import { createParser } from "eventsource-parser";
 
 export default function Home() {
   const SYSTEM_MESSAGE="You are Brobot an extremely sarcastic but helpful AI built with state of the art large language models"
@@ -11,39 +13,83 @@ export default function Home() {
   
   const API_URL = "https://api.openai.com/v1/chat/completions";
 
-
-  async function sendRequest(){
-    // update the message history
-const newMessage = { role: "user", content: userMessage};
-const newMessages = [  ...messages, newMessage];
-setMessages(newMessages);
-setUserMessage("");
-
-    const response = await fetch(API_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + apiKey,
+  // update the message history
+  const sendRequest = async () => {
+    const updatedMessages = [
+      ...messages,
+      {
+        role: "user",
+        content: userMessage,
       },
-      body: JSON.stringify({
-        "model": "gpt-3.5-turbo",
-        "messages": newMessages,
-      }),
-    });
-    const responseJson = await response.json();
-    
-    const newBotMessage = responseJson.choices[0].message;
+    ];
 
-    const newMessages2 = [...newMessages, newBotMessage];
+    setMessages(updatedMessages);
+    setUserMessage("");
 
-    setMessages(newMessages2);
-  }
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: updatedMessages,
+          stream: true,
+        }),
+      });
 
+      const reader = response.body.getReader();
+
+      let newMessage = "";
+      const parser = createParser((event) => {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            return;
+          }
+          const json = JSON.parse(event.data);
+          const content = json.choices[0].delta.content;
+
+          if (!content) {
+            return;
+          }
+
+          newMessage += content;
+
+          const updatedMessages2 = [
+            ...updatedMessages,
+            { role: "assistant", content: newMessage },
+          ];
+
+          setMessages(updatedMessages2);
+        } else {
+          return "";
+        }
+      });
+
+      // eslint-disable-next-line
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const text = new TextDecoder().decode(value);
+        parser.feed(text);
+      }
+    } catch (error) {
+      console.error("error");
+      window.alert("Error:" + error.message);
+      }
+  };
   
    
   /* Add  more logic here */
 
   return (
+    <>
+    <Head>
+      <title>Brobot - AI Thing</title>
+    </Head>
     <div className="flex flex-col h-screen">
       {/* Navbar */}
       <nav className="bg-white shadow w-full">
@@ -89,5 +135,6 @@ setUserMessage("");
         </div>
       </div>
    </div>
+    </>
   );
 }
